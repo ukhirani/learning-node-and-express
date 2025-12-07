8121fb1f9f40ae3b8db1da03c491927b0c14ae99
Umang Hirani <umanghirani.exe@gmail.com>
2025-12-07 21:23:38 +0530

    learnt about processes and environments

---
+++ node/codes/process.js
@@ -0,0 +1,9 @@
+// get the arguments of this command process
+console.log(process.argv);
+
+// get the process id
+console.log(process.pid);
+
+// get the process ENV
+// here's how you get the API_KEY's
+console.log(process.env.NODE_ENV);
