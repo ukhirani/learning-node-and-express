4f001067ae70a0f62bf74aaff929c2ba73808162
Umang Hirani <umanghirani.exe@gmail.com>
2026-02-10 12:52:18 +0530

    cli chat : now working with multiple users and proper logging
    also input override is handled when a user is typing and another user
    has sent a message

--- ws-chat/client/client.js
+++ ws-chat/client/client.js
@@ -8,28 +8,57 @@ const rl = readline.createInterface({
 
 const socket = new WebSocket("ws://localhost:8080");
 
+let username = "";
+
 socket.onopen = () => {
   console.log("Connected to the WebSocket server");
 
-  let username;
   rl.question("What is your name? ", (name) => {
     username = name;
+
     socket.send(
       JSON.stringify({
         type: "registerUser",
         username: name,
       }),
     );
-    rl.close();
+
+    console.log("You can now start chatting:");
+
+    rl.setPrompt("> ");
+    rl.prompt();
+    rl.on("line", (input) => {
+      rl.setPrompt("> ");
+      rl.prompt();
+
+      if (input.trim().length > 0) {
+        socket.send(
+          JSON.stringify({
+            type: "chatMessage",
+            message: input,
+          }),
+        );
+      }
+    });
   });
 };
 
 socket.onmessage = (message) => {
-  console.log("Received from the server:", message.data.toString());
+  const currentLine = rl.line;
+  const cursorPos = rl.cursor;
+
+  readline.clearLine(process.stdout, 0);
+  readline.cursorTo(process.stdout, 0);
+
+  console.log(message.data.toString());
+
+  rl.prompt(true);
+  readline.cursorTo(process.stdout, rl.getPrompt().length + cursorPos);
 };
 
 socket.onclose = () => {
   console.log("Disconnected from the server");
+  rl.close();
 };
 
 socket.onerror = (error) => {
--- ws-chat/server/server.js
+++ ws-chat/server/server.js
@@ -5,26 +5,41 @@ let userMap = new Map();
 
 console.log("WebSocket server running on ws://localhost:8080");
 
+function SendMessageFromUser(username, ws, message) {
+  for (const key of userMap.keys()) {
+    if (key !== ws) {
+      key.send("[" + username + "] " + message);
+    }
+  }
+}
+
 wss.on("connection", (ws) => {
   console.log("Client connected");
 
   ws.on("message", (data) => {
     const message = data.toString();
     let jsonData = JSON.parse(message);
+    console.log(jsonData);
 
     switch (jsonData.type) {
       case "registerUser":
         userMap.set(ws, jsonData.username);
-        ws.send("[SERVER] + " + jsonData.username + " joined the chat");
+        SendMessageFromUser(
+          "SERVER",
+          ws,
+          jsonData.username + " joined the chat",
+        );
         break;
 
-      default:
-        console.log("default case");
+      case "chatMessage":
+        const user = userMap.get(ws);
+        SendMessageFromUser(user, ws, jsonData.message);
         break;
     }
   });
 
   ws.on("close", () => {
+    SendMessageFromUser("SERVER", ws, userMap.get(ws) + " left the chat.");
     console.log("Client disconnected");
   });
 });
